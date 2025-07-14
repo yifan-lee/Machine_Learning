c761b3bc002663432b19f361d6b9462b96007e92
yifan-lee <ivanlee142857@gmail.com>
2025-07-13 22:04:46 -0400

    why SimplifiedNN is better than flixNN2?

--- code/basic_NN.py
+++ code/basic_NN.py
@@ -23,13 +23,12 @@ class BasicNN_baseline(nn.Module):
         hiddenLayerNode2_0 = input * self.w10 + self.b10
         hiddenLayerNode2_1 = F.relu(hiddenLayerNode2_0)
 
-        hiddenLayerNode3_0 = (
+        output = F.relu(
             self.w01 * hiddenLayerNode1_1
             + self.w11 * hiddenLayerNode2_1
             + self.final_bias
         )
 
-        output = F.relu(hiddenLayerNode3_0)
         return output
 
 
@@ -51,13 +50,12 @@ class BasicNN(nn.Module):
         hiddenLayerNode2_0 = input * self.w10 + self.b10
         hiddenLayerNode2_1 = F.relu(hiddenLayerNode2_0)
 
-        hiddenLayerNode3_0 = (
+        output = F.relu(
             self.w01 * hiddenLayerNode1_1
             + self.w11 * hiddenLayerNode2_1
             + self.final_bias
         )
 
-        output = F.relu(hiddenLayerNode3_0)
         return output
 
 
@@ -76,6 +74,7 @@ class SimplifiedNN(nn.Module):
         return output
 
 
+
 class BetterNN(nn.Module):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -91,4 +90,109 @@ class BetterNN(nn.Module):
         y1 = F.relu(x @ self.W1 + self.B1)
         y2 = F.relu(y1 @ self.W2 + self.B2)
         output = F.relu(y2 @ self.W3 + self.B3)
+        # output = (y2 @ self.W3 + self.B3)
         return output
+
+class flixNN(nn.Module):
+    def __init__(self, dim1=16,dim2=16,*args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.W1 = nn.Parameter(torch.randn(1, dim1))
+        self.B1 = nn.Parameter(torch.randn(dim1))
+        self.W2 = nn.Parameter(torch.randn(dim1, dim2))
+        self.B2 = nn.Parameter(torch.randn(dim2))
+        self.W3 = nn.Parameter(torch.randn(dim2, 1))
+        self.B3 = nn.Parameter(torch.randn(1))
+
+    def forward(self, input):
+        x = input.view(-1, 1)
+        y1 = F.relu(x @ self.W1 + self.B1)
+        y2 = F.relu(y1 @ self.W2 + self.B2)
+        output = F.relu(y2 @ self.W3 + self.B3)
+        # output = (y2 @ self.W3 + self.B3)
+        return output
+    
+class flixNN2(nn.Module):
+    def __init__(self, dim1=2,*args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.W1 = nn.Parameter(torch.randn(1, dim1))
+        self.B1 = nn.Parameter(torch.randn(dim1))
+        self.W3 = nn.Parameter(torch.randn(dim1, 1))
+        self.B3 = nn.Parameter(torch.randn(1))
+
+    def forward(self, input):
+        x = input.view(-1, 1)
+        y1 = F.relu(x @ self.W1 + self.B1)
+        output = F.relu(y1 @ self.W3 + self.B3)
+        return output
+    
+    
+# import numpy as np
+# import pandas as pd
+
+
+# import torch
+# import torch.nn as nn
+# import torch.nn.functional as F  # activation function ReLU
+# from torch.optim import SGD  # stochastic gradient descent
+
+
+# torch.manual_seed(0)
+# x_train = torch.linspace(0, 1, 1000)
+# noise_train = torch.randn(1000) * 0.1
+# y_train = (torch.sin(np.pi * x_train) > 0.5).float() + noise_train  # 二元分类目标
+
+# x_test = torch.linspace(0.05, 0.95, 20)
+# noise_test = torch.randn(20) * 0.1
+# y_test = (torch.sin(np.pi * x_test) > 0.5).float() + noise_test
+
+
+# ### SimplifiedNN
+
+
+# model = SimplifiedNN()
+# criterion = nn.MSELoss()
+# optimizer = SGD(model.parameters(), lr=0.1)
+
+
+# n_epochs = 1000
+# loss_history = []
+
+# for epoch in range(n_epochs):
+#     # forward pass
+#     y_pred = model(x_train).view(-1)
+#     loss = criterion(y_pred, y_train)
+#     loss_history.append(loss.item())
+
+#     # backward pass
+#     optimizer.zero_grad()
+#     loss.backward()
+#     optimizer.step()
+
+# y_test_pred = model(x_test).view(-1)
+# lossTest = criterion(y_test_pred, y_test)
+# print(f"MSE (Train) for Simplified NN model is {loss.item():.4f}")
+# print(f"MSE (Test) for Simplified NN model is {lossTest.item():.4f}")
+
+# model = flixNN2(dim1=2)
+# criterion = nn.MSELoss()
+# optimizer = SGD(model.parameters(), lr=0.1)
+
+
+# n_epochs = 1000
+# loss_history = []
+
+# for epoch in range(n_epochs):
+#     # forward pass
+#     y_pred = model(x_train).view(-1)
+#     loss = criterion(y_pred, y_train)
+#     loss_history.append(loss.item())
+
+#     # backward pass
+#     optimizer.zero_grad()
+#     loss.backward()
+#     optimizer.step()
+
+# y_test_pred = model(x_test).view(-1)
+# lossTest = criterion(y_test_pred, y_test)
+# print(f"MSE (Train) for Better NN model is {loss.item():.4f}")
+# print(f"MSE (Test) for Better NN model is {lossTest.item():.4f}")
--- code/main.py
+++ code/main.py
@@ -8,18 +8,20 @@ import torch.nn.functional as F  # activation function ReLU
 from torch.optim import SGD  # stochastic gradient descent
 
 
-from basic_NN import BasicNN_baseline, BasicNN, SimplifiedNN, BetterNN
+from basic_NN import BasicNN_baseline, BasicNN, SimplifiedNN, BetterNN, flixNN, flixNN2
 
 
-x_train = torch.linspace(0, 1, steps=100)
-y_train = torch.sin(torch.pi * x_train)
 
 
-x_test = torch.linspace(0.05, 0.95, steps=10)
-torch.manual_seed(42)  # 保证可复现
-noise = torch.randn(10) * 0.1
-y_test = torch.sin(torch.pi * x_test) + noise
 
+torch.manual_seed(0)
+x_train = torch.linspace(0, 1, 1000)
+noise_train = torch.randn(1000) * 0.1
+y_train = (torch.sin(np.pi * x_train) > 0.5).float() + noise_train  # 二元分类目标
+
+x_test = torch.linspace(0.05, 0.95, 20)
+noise_test = torch.randn(20) * 0.1
+y_test = (torch.sin(np.pi * x_test) > 0.5).float() + noise_test
 
 ### BasicNN_baseline
 
@@ -27,14 +29,13 @@ model = BasicNN_baseline()
 output_values = model(x_test)
 
 testLoss = F.mse_loss(output_values, y_test)
-print(f"MSE for baseline NN model is {testLoss}")
+print(f"MSE for baseline NN model is {testLoss:.4f}")
 
 ### BasicNN
 
 model = BasicNN()
 criterion = nn.MSELoss()
 optimizer = SGD(model.parameters(), lr=0.1)
-
 
 n_epochs = 1000
 loss_history = []
@@ -49,9 +50,6 @@ for epoch in range(n_epochs):
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
-
-    # if epoch % 100 == 0:
-    #     print(f"Epoch {epoch}: Loss = {loss.item():.4f}")
 
 y_test_pred = model(x_test)
 lossTest = criterion(y_test_pred, y_test)
@@ -95,6 +93,61 @@ print(f"MSE (Test) for Simplified NN model is {lossTest.item():.4f}")
 model = BetterNN()
 criterion = nn.MSELoss()
 optimizer = SGD(model.parameters(), lr=0.1)
+
+
+n_epochs = 1000
+loss_history = []
+
+for epoch in range(n_epochs):
+    # forward pass
+    y_pred = model(x_train).view(-1)
+    loss = criterion(y_pred, y_train)
+    loss_history.append(loss.item())
+
+    # backward pass
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+y_test_pred = model(x_test).view(-1)
+lossTest = criterion(y_test_pred, y_test)
+print(f"MSE (Train) for Better NN model is {loss.item():.4f}")
+print(f"MSE (Test) for Better NN model is {lossTest.item():.4f}")
+
+### flixNN
+
+
+model = flixNN(dim1=4,dim2=4)
+criterion = nn.MSELoss()
+optimizer = SGD(model.parameters(), lr=0.1)
+
+
+n_epochs = 1000
+loss_history = []
+
+for epoch in range(n_epochs):
+    # forward pass
+    y_pred = model(x_train).view(-1)
+    loss = criterion(y_pred, y_train)
+    loss_history.append(loss.item())
+
+    # backward pass
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+y_test_pred = model(x_test).view(-1)
+lossTest = criterion(y_test_pred, y_test)
+print(f"MSE (Train) for Better NN model is {loss.item():.4f}")
+print(f"MSE (Test) for Better NN model is {lossTest.item():.4f}")
+
+
+### flixNN2
+
+
+model = flixNN2(dim1=2)
+criterion = nn.MSELoss()
+optimizer = SGD(model.parameters(), lr=0.01)
 
 
 n_epochs = 1000
