b6dc72cf0448bf7e28841a532ba6d640a0eceadc
yifan-lee <ivanlee142857@gmail.com>
2025-07-14 09:15:07 -0400

    add test_model function to avoid repeating writing testing code

--- code/main.py
+++ code/main.py
@@ -36,185 +36,109 @@ x_test = torch.linspace(0.05, 0.95, 20)
 noise_test = torch.randn(20) * 0.1
 y_test = (torch.sin(np.pi * x_test) > 0.5).float() + noise_test
 
+dat_1dim = {
+    'x_train':x_train,
+    'y_train':y_train,
+    'x_test':x_test,
+    'y_test':y_test
+}
 
-x2_train = torch.rand(1000, 2)
-y2_train = (((torch.sin(np.pi * x2_train[:, 0]) + torch.cos(np.pi * x2_train[:, 1])) > 1.0).float() + torch.randn(1000) * 0.1)
+x_train = torch.rand(1000, 2)
+y_train = (((torch.sin(np.pi * x_train[:, 0]) + torch.cos(np.pi * x_train[:, 1])) > 1.0).float() + torch.randn(1000) * 0.1)
 
-x2_test = torch.rand(200, 2)
-y2_test = (((torch.sin(np.pi * x2_test[:, 0]) + torch.cos(np.pi * x2_test[:, 1])) > 1.0).float() + torch.randn(200) * 0.1)
+x_test = torch.rand(200, 2)
+y_test = (((torch.sin(np.pi * x_test[:, 0]) + torch.cos(np.pi * x_test[:, 1])) > 1.0).float() + torch.randn(200) * 0.1)
 
-# ### BasicNN_baseline
+dat_2dim = {
+    'x_train':x_train,
+    'y_train':y_train,
+    'x_test':x_test,
+    'y_test':y_test
+}
 
-# model = BasicNN_baseline()
-# output_values = model(x_test)
+def test_model(model, data, n_epochs):
+    x_train = data['x_train']
+    y_train = data['y_train']
+    x_test = data['x_test']
+    y_test = data['y_test']
+    
+    criterion = nn.MSELoss()
+    optimizer = SGD(model.parameters(), lr=0.1)
+    loss_history = []
+    
+    for epoch in range(n_epochs):
+        # forward pass
+        y_pred = model(x_train).view(-1)
+        loss = criterion(y_pred, y_train)
+        loss_history.append(loss.item())
 
-# testLoss = F.mse_loss(output_values, y_test)
-# print(f"MSE for baseline NN model is {testLoss:.4f}")
+        # backward pass
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+    y_test_pred = model(x_test).view(-1)
+    lossTest = criterion(y_test_pred, y_test)
+    print(f"MSE (Train) for baseline NN model is {loss.item():.4f}")
+    print(f"MSE (Test) for baseline NN model is {lossTest.item():.4f}")
+    
+    return 0
+
+# ## 1 dim
+
+# # ### BasicNN_baseline
+
+# # model = BasicNN_baseline()
+# # output_values = model(x_test)
+
+# # testLoss = F.mse_loss(output_values, y_test)
+# # print(f"MSE for baseline NN model is {testLoss:.4f}")
+
+
+
+# n_epochs = 1000
+# data = dat_1dim
 
 # ### BasicNN
 
 # model = BasicNN()
-# criterion = nn.MSELoss()
-# optimizer = SGD(model.parameters(), lr=0.1)
+# test_model(model, data, n_epochs)
 
-# n_epochs = 1000
-# loss_history = []
-
-# for epoch in range(n_epochs):
-#     # forward pass
-#     y_pred = model(x_train)
-#     loss = criterion(y_pred, y_train)
-#     loss_history.append(loss.item())
-
-#     # backward pass
-#     optimizer.zero_grad()
-#     loss.backward()
-#     optimizer.step()
-
-# y_test_pred = model(x_test)
-# lossTest = criterion(y_test_pred, y_test)
-# print(f"MSE (Train) for baseline NN model is {loss.item():.4f}")
-# print(f"MSE (Test) for baseline NN model is {lossTest.item():.4f}")
-
-# # print("== BasicNN 参数 ==")
-# # for name, param in model.named_parameters():
-# #     print(f"{name}: {param.data}")
 
 # ### SimplifiedNN
 
-
 # model = SimplifiedNN()
-# criterion = nn.MSELoss()
-# optimizer = SGD(model.parameters(), lr=0.1)
+# test_model(model, data, n_epochs)
 
-
-# n_epochs = 1000
-# loss_history = []
-
-# for epoch in range(n_epochs):
-#     # forward pass
-#     y_pred = model(x_train).view(-1)
-#     loss = criterion(y_pred, y_train)
-#     loss_history.append(loss.item())
-
-#     # backward pass
-#     optimizer.zero_grad()
-#     loss.backward()
-#     optimizer.step()
-
-# y_test_pred = model(x_test).view(-1)
-# lossTest = criterion(y_test_pred, y_test)
-# print(f"MSE (Train) for Simplified NN model is {loss.item():.4f}")
-# print(f"MSE (Test) for Simplified NN model is {lossTest.item():.4f}")
 
 # ### BetterNN
 
-
 # model = BetterNN()
-# criterion = nn.MSELoss()
-# optimizer = SGD(model.parameters(), lr=0.1)
-
-
-# n_epochs = 1000
-# loss_history = []
-
-# for epoch in range(n_epochs):
-#     # forward pass
-#     y_pred = model(x_train).view(-1)
-#     loss = criterion(y_pred, y_train)
-#     loss_history.append(loss.item())
-
-#     # backward pass
-#     optimizer.zero_grad()
-#     loss.backward()
-#     optimizer.step()
-
-# y_test_pred = model(x_test).view(-1)
-# lossTest = criterion(y_test_pred, y_test)
-# print(f"MSE (Train) for Better NN model is {loss.item():.4f}")
-# print(f"MSE (Test) for Better NN model is {lossTest.item():.4f}")
+# test_model(model, data, n_epochs)
 
 # ### flixNN
 
-
 # model = flixNN(dim1=4,dim2=4)
-# criterion = nn.MSELoss()
-# optimizer = SGD(model.parameters(), lr=0.1)
+# test_model(model, data, n_epochs)
 
-
-# n_epochs = 1000
-# loss_history = []
-
-# for epoch in range(n_epochs):
-#     # forward pass
-#     y_pred = model(x_train).view(-1)
-#     loss = criterion(y_pred, y_train)
-#     loss_history.append(loss.item())
-
-#     # backward pass
-#     optimizer.zero_grad()
-#     loss.backward()
-#     optimizer.step()
-
-# y_test_pred = model(x_test).view(-1)
-# lossTest = criterion(y_test_pred, y_test)
-# print(f"MSE (Train) for Better NN model is {loss.item():.4f}")
-# print(f"MSE (Test) for Better NN model is {lossTest.item():.4f}")
-
-
-### flixNN2
+# ### flixNN2
 
 
 # model = flixNN2(dim1=5)
-# criterion = nn.MSELoss()
-# optimizer = SGD(model.parameters(), lr=0.01)
+# test_model(model, data, n_epochs)
 
 
-# n_epochs = 1000
-# loss_history = []
 
-# for epoch in range(n_epochs):
-#     # forward pass
-#     y_pred = model(x_train).view(-1)
-#     loss = criterion(y_pred, y_train)
-#     loss_history.append(loss.item())
 
-#     # backward pass
-#     optimizer.zero_grad()
-#     loss.backward()
-#     optimizer.step()
+## 2 dim
 
-# y_test_pred = model(x_test).view(-1)
-# lossTest = criterion(y_test_pred, y_test)
-# print(f"MSE (Train) for Better NN model is {loss.item():.4f}")
-# print(f"MSE (Test) for Better NN model is {lossTest.item():.4f}")
-
+data = dat_2dim
+n_epochs = 1000
 
 ### SimplifiedNN_dim2
 
-
 model = SimplifiedNN_dim2(dim1=3)
-criterion = nn.MSELoss()
-optimizer = SGD(model.parameters(), lr=0.01)
+test_model(model, data, n_epochs)
 
-
-n_epochs = 1000
-loss_history = []
-
-for epoch in range(n_epochs):
-    # forward pass
-    y2_pred = model(x2_train).view(-1)
-    loss = criterion(y2_pred, y2_train)
-    loss_history.append(loss.item())
-
-    # backward pass
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-
-y2_test_pred = model(x2_test).view(-1)
-lossTest = criterion(y2_test_pred, y2_test)
-print(f"MSE (Train) for SimplifiedNN_dim2 model is {loss.item():.4f}")
-print(f"MSE (Test) for SimplifiedNN_dim2 model is {lossTest.item():.4f}")
 
 print("Success!")
