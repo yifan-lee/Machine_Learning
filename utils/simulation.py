2e668a6064e969e2c90f011572eebb1c1b6d64a0
yifan-lee <ivanlee142857@gmail.com>
2025-07-28 18:09:57 -0400

    finish simulation of 1d1d and ndnc

--- utils/simulation.py
+++ utils/simulation.py
@@ -13,20 +13,28 @@ from generate_ndnc_csv import generate_ndnc_csv
 if __name__ == "__main__":
     simulationData = generate_1d1d_csv(trainSize=1000, testSize=200, seed=0)
 
-    XcombinedDf = combine_train_test_data(simulationData['xTrain'], simulationData['xTest'])
-    YcombinedDf = combine_train_test_data(simulationData['yTrain'], simulationData['yTest'])
-    
-    
+    xTrain = pd.DataFrame(simulationData['xTrain'].numpy())
+    yTrain = pd.DataFrame(simulationData['yTrain'].numpy())
+    xTest = pd.DataFrame(simulationData['xTest'].numpy())
+    yTest = pd.DataFrame(simulationData['yTest'].numpy())
 
-    XcombinedDf.to_csv('./data/x_1d1d.csv', index=False)
-    YcombinedDf.to_csv('./data/y_1d1d.csv', index=False)
-    print("CSV x_1d1d.csv 和 y_1d1d.csv")
+    xTrain.to_csv('./data/x_train_1d1d.csv', index=False)
+    yTrain.to_csv('./data/y_train_1d1d.csv', index=False)
+    xTest.to_csv('./data/x_test_1d1d.csv', index=False)
+    yTest.to_csv('./data/y_test_1d1d.csv', index=False)
+    print("Save 1d1d")
     
-    # simulationData = generate_ndnc_csv(inputDim=3, outputClass=10, trainSize=1000, testSize=200, seed=0)
-    # trainDf = pd.DataFrame({'x': simulationData['xTrain'].numpy(), 'y': simulationData['yTrain'].numpy()})
-    # testDf = pd.DataFrame({'x': simulationData['xTest'].numpy(), 'y': simulationData['yTest'].numpy()})
-    
-    # trainDf.to_csv('./data/train_ndnc.csv', index=False)
-    # testDf.to_csv('./data/test_ndnc.csv', index=False)
-    # print("CSV train_ndnc.csv 和 test_ndnc.csv")
+    inputDim = 3
+    outputClass = 10
+    simulationData = generate_ndnc_csv(inputDim=inputDim, outputClass=outputClass, trainSize=1000, testSize=200, seed=0)
+    xTrain = pd.DataFrame(simulationData['xTrain'].numpy())
+    yTrain = pd.DataFrame(simulationData['yTrain'].numpy())
+    xTest = pd.DataFrame(simulationData['xTest'].numpy())
+    yTest = pd.DataFrame(simulationData['yTest'].numpy())
+    xTrain.to_csv('./data/x_train_ndnc.csv', index=False)
+    yTrain.to_csv('./data/y_train_ndnc.csv', index=False)
+    xTest.to_csv('./data/x_test_ndnc.csv', index=False)
+    yTest.to_csv('./data/y_test_ndnc.csv', index=False)
+
+    print("Save ndnc")
 
