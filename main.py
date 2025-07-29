730de28c11a1e3fa8729a85a6b2a700973245f36
yifan-lee <ivanlee142857@gmail.com>
2025-07-29 16:05:49 -0400

    test old 2dim model

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from torch.optim import SGD  # stochastic gradient descent
 from utils.train_model import train
 from utils.eval_model import evaluate
 from model.basic_NN import BasicNN_baseline, BasicNN, SimplifiedNN, BetterNN, flixNN, flixNN2
-from model.basic_NN_2dim_inputs import SimplifiedNN_dim2, SimplifiedNN_dim2_layer2
-from model.basic_NN_2dim_inputs_3dim_outputs import SimplifiedNN_dim2dim3, SimplifiedNN_dim2dim3_layer2
+from model.basic_NN_2dim_inputs_1dim_outputs import SimplifiedNN_dim2, SimplifiedNN_dim2_layer2
+from model.basic_NN_2dim_inputs_3dim_1class_outputs import SimplifiedNN_dim2dim3, SimplifiedNN_dim2dim3_layer2
 
 
 
@@ -25,39 +25,65 @@ yTrain = torch.tensor(yTrain, dtype=torch.float32)
 xTest = torch.tensor(xTest, dtype=torch.float32)
 yTest = torch.tensor(yTest, dtype=torch.float32)
 
+xTrain2d = pd.read_csv('data/x_train_2d1d.csv', header=None).values
+yTrain2d = pd.read_csv('data/y_train_2d1d.csv', header=None).values
+xTest2d = pd.read_csv('data/x_test_2d1d.csv', header=None).values
+yTest2d = pd.read_csv('data/y_test_2d1d.csv', header=None).values
+
+xTrain2d = torch.tensor(xTrain2d, dtype=torch.float32)
+yTrain2d = torch.tensor(yTrain2d, dtype=torch.float32)
+xTest2d = torch.tensor(xTest2d, dtype=torch.float32)
+yTest2d = torch.tensor(yTest2d, dtype=torch.float32)
+
 
 ## 1 dim
 
-epochs = 1000
-criterion=torch.nn.MSELoss()
+if 0:
+    epochs = 1000
+    criterion=torch.nn.MSELoss()
 
-model = BasicNN()
-optimizer=SGD(model.parameters(), lr=0.01)
-modelTrained = train(model, xTrain, yTrain, optimizer, criterion, epochs)
-loss = evaluate(model, xTest, yTest, criterion)
-print(f"MSE for baseline NN model is {loss:.4f}")
+    model = BasicNN()
+    optimizer=SGD(model.parameters(), lr=0.01)
+    modelTrained = train(model, xTrain, yTrain, optimizer, criterion, epochs)
+    loss = evaluate(model, xTest, yTest, criterion)
+    print(f"MSE for baseline NN model is {loss:.4f}")
 
-model = SimplifiedNN()
-optimizer=SGD(model.parameters(), lr=0.01)
-modelTrained = train(model, xTrain, yTrain, optimizer, criterion, epochs)
-loss = evaluate(model, xTest, yTest, criterion)
-print(f"MSE for baseline SimplifiedNN model is {loss:.4f}")
+    model = SimplifiedNN()
+    optimizer=SGD(model.parameters(), lr=0.01)
+    modelTrained = train(model, xTrain, yTrain, optimizer, criterion, epochs)
+    loss = evaluate(model, xTest, yTest, criterion)
+    print(f"MSE for baseline SimplifiedNN model is {loss:.4f}")
 
-model = BetterNN()
-optimizer=SGD(model.parameters(), lr=0.01)
-modelTrained = train(model, xTrain, yTrain, optimizer, criterion, epochs)
-loss = evaluate(model, xTest, yTest, criterion)
-print(f"MSE for baseline BetterNN model is {loss:.4f}")
+    model = BetterNN()
+    optimizer=SGD(model.parameters(), lr=0.01)
+    modelTrained = train(model, xTrain, yTrain, optimizer, criterion, epochs)
+    loss = evaluate(model, xTest, yTest, criterion)
+    print(f"MSE for baseline BetterNN model is {loss:.4f}")
 
-model = flixNN(dim1=4,dim2=4)
-optimizer=SGD(model.parameters(), lr=0.01)
-modelTrained = train(model, xTrain, yTrain, optimizer, criterion, epochs)
-loss = evaluate(model, xTest, yTest, criterion)
-print(f"MSE for baseline flixNN model is {loss:.4f}")
+    model = flixNN(dim1=4,dim2=4)
+    optimizer=SGD(model.parameters(), lr=0.01)
+    modelTrained = train(model, xTrain, yTrain, optimizer, criterion, epochs)
+    loss = evaluate(model, xTest, yTest, criterion)
+    print(f"MSE for baseline flixNN model is {loss:.4f}")
 
 
 
 ## 2 dim
+
+epochs = 1000
+criterion=torch.nn.MSELoss()
+
+model = SimplifiedNN_dim2(dim1=3)
+optimizer=SGD(model.parameters(), lr=0.01)
+modelTrained = train(model, xTrain2d, yTrain2d, optimizer, criterion, epochs)
+loss = evaluate(model, xTest2d, yTest2d, criterion)
+print(f"MSE for baseline SimplifiedNN_dim2 model is {loss:.4f}")
+
+model = SimplifiedNN_dim2_layer2(dim1=2, dim2=5)
+optimizer=SGD(model.parameters(), lr=0.01)
+modelTrained = train(model, xTrain2d, yTrain2d, optimizer, criterion, epochs)
+loss = evaluate(model, xTest2d, yTest2d, criterion)
+print(f"MSE for baseline SimplifiedNN_dim2_layer2 model is {loss:.4f}")
 
 # if model_class == 'dim2':
 #     data = dat_2dim
@@ -65,8 +91,7 @@ print(f"MSE for baseline flixNN model is {loss:.4f}")
 
 #     ### SimplifiedNN_dim2
 
-#     model = SimplifiedNN_dim2(dim1=3)
-#     test_model(model, data, n_epochs)
+#     
 
 #     ### SimplifiedNN_dim2_layer2
 
--- model/basic_NN_2dim_inputs_1dim_outputs.py
+++ model/basic_NN_2dim_inputs_1dim_outputs.py
@@ -34,3 +34,21 @@ class SimplifiedNN_dim2_layer2(nn.Module):
         y2 = F.relu(y1 @ self.W2 + self.B2)
         output = F.relu(y2 @ self.W3 + self.B3)
         return output
+
+
+class flexNN_dim2(nn.Module):
+    def __init__(self, dim1=2,dim2=2,*args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.W1 = nn.Parameter(torch.randn(2, dim1))
+        self.B1 = nn.Parameter(torch.randn(dim1))
+        self.W2 = nn.Parameter(torch.randn(dim1, dim2))
+        self.B2 = nn.Parameter(torch.randn(dim2))
+        self.W3 = nn.Parameter(torch.randn(dim2, 1))
+        self.B3 = nn.Parameter(torch.randn(1))
+
+    def forward(self, input):
+        x = input.view(-1, 2)
+        y1 = F.relu(x @ self.W1 + self.B1)
+        y2 = F.relu(y1 @ self.W2 + self.B2)
+        output = F.relu(y2 @ self.W3 + self.B3)
+        return output
