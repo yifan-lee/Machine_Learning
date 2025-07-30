4d457a796f26701502e9135a5e0a517782f1ff83
yifan-lee <ivanlee142857@gmail.com>
2025-07-30 16:21:18 -0400

    1. finish CNN 2. finish train function for CNN 3. add evaluate_CNN function

--- model/CNN.py
+++ model/CNN.py
@@ -1,3 +1,23 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F  # activation function ReLU
+
+class CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 2, kernel_size=3, padding=1),  # 2D convolution
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),  # 2D max pooling
+        )
+        self.fc = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(2 * 14 * 14, 64),  # Adjust input size based on the output of conv layer
+            nn.ReLU(),
+            nn.Linear(64, 26)  # Output layer for 26 classes
+        )
+        
+    def forward(self, x):
+        x = self.conv(x) 
+        x = self.fc(x)
+        return x
--- utils/eval_model.py
+++ utils/eval_model.py
@@ -4,3 +4,28 @@ def evaluate(model, x, y, criterion):
     yPred = model(x)
     loss = criterion(yPred, y)
     return loss
+
+
+
+def evaluate_CNN(model, x, y=None, criterion=None, device='mps'):
+    if y is None:
+        dataLoader = x
+    else:
+        dataLoader = torch.utils.data.DataLoader(
+            torch.utils.data.TensorDataset(x, y),
+            batch_size=32,
+            shuffle=False
+        )
+        
+    model.eval()
+    totalCount = 0
+    correctCount = 0
+    with torch.no_grad():
+        for x, y in dataLoader:
+            x = x.to(device)
+            y = y.to(device)
+            output = model(x)
+            pred = output.argmax(dim=1)
+            correctCount += (pred == y).sum().item()
+            totalCount += y.size(0)
+    return correctCount / totalCount if totalCount > 0 else 0
